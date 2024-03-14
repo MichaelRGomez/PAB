10474a85b42200691e91f49d93f4e30d41f9639e
Michael <2018118090@ub.edu.bz>
2024-03-14 16:31:34 -0600

    finished creating art schema and model

---
+++ src/art-module/Models/art.ts
@@ -0,0 +1,39 @@
+import ArtModel, { Art, NewArt } from "../schemas/art";
+
+export default class Arts{
+    //CREATE
+    static async create(data: NewArt): Promise <Art>{
+        return await ArtModel.create(data);
+    };
+
+    //READ
+    static async getById(id: string): Promise<Art | null>{
+        return await ArtModel.
+        findById(id)
+        .populate({path: 'Files'});
+    };
+
+    static async getAll():Promise<Art[]>{
+        const folder: Art[] = await ArtModel.find()
+        .populate({path: 'Files'});
+        return folder;
+    };
+
+    static async getAllByTags(tagArr: string[]):Promise<Art[]>{
+        const folder: Art[] = await ArtModel.find({tags: {$in: tagArr}}).
+        populate({path: 'Files'});
+        return folder;
+    }
+
+    //UPDATE
+    static async removeTag(tagToBeRemoved: string, art: Art): Promise<Art | null>{
+        const { id } = art;
+        const oldTags: string[] = art.tags!;
+        const newTags: string[] = oldTags.filter(el => el !== tagToBeRemoved);
+        art.tags = newTags;
+
+        return await ArtModel.findByIdAndUpdate(id, art);
+    }
+
+    //DELETE
+}
---
+++ src/art-module/schemas/art.ts
@@ -0,0 +1,37 @@
+//3rd party import
+import mongoose, {Schema} from "mongoose";
+
+const { ObjectId } = Schema.Types;
+
+//Interface type for Art Document
+export type Art = {
+    id: string;
+    title: string;
+    file: string | null; 
+    caption?: string | null;
+    tags?: string[];
+}
+
+//New version
+export type NewArt = Omit<Art, 'id'>;
+
+//Art Schema
+const ArtSchema: Schema = new Schema({
+    title: { type: String, required: true},
+    file: { type: ObjectId, required: true, ref: 'Files'},
+    caption: {type: String, required: false},
+    tags: { type: [String], required: false},
+},{collection: 'Art', toJSON: {virtuals: true}, toObject: {virtuals: true}});
+
+//Virtuals
+ArtSchema.virtual('public_art',{
+    ref: 'Files',
+    localField: 'file',
+    foreignField: '_id',
+    justOne: true,
+});
+
+const ArtModel = mongoose.model<Art>('Art', ArtSchema);
+
+export default ArtModel;
+
